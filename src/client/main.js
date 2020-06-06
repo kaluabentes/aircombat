b3150771f19bebef80a5731d28095734c4136b6d
Kaluã Bentes <kaluanbentes@gmail.com>
2020-06-06 00:09:56 -0300

    refactor(canvas): improve image quality

--- src/client/main.js
+++ src/client/main.js
@@ -3,8 +3,8 @@ import planeSprite from "./assets/sprites/fighter-plane.png";
 const planeSourceWidth = 245;
 const planeSourcHeight = 350;
 const planeSourceY = 0;
-const planeWidth = 146;
-const planeHeight = 205;
+const planeWidth = 245;
+const planeHeight = 350;
 
 let frames = [0, 245, 490];
 let frame = 0;
@@ -14,7 +14,7 @@ let canvas;
 
 function drawPlane() {
   const planeX = canvas.width * 0.5 - planeWidth * 0.5;
-  const planeY = canvas.height - planeHeight;
+  const planeY = canvas.height - planeHeight - 30;
 
   context.drawImage(
     planeImage,
@@ -49,8 +49,8 @@ function main() {
   canvas = document.getElementById("canvas");
   context = canvas.getContext("2d");
 
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth * 2;
+  canvas.height = window.innerHeight * 2;
 
   planeImage = new Image();
   planeImage.setAttribute("src", planeSprite);
